e6aec69d82674931bdb8546cdcbcb97cb10dca59
Amit Ashdot <amitashdot@gmail.com>
2020-08-11 17:05:14 +0300

    Updates

---
+++ precache-manifest.96ad730a3d24d4319d29311fd5630a01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3298006a9aa28111724706748b74c9dd",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "adbb3824a4d709caf00e",
+    "url": "/homepage/static/css/main.e9e2a498.chunk.css"
+  },
+  {
+    "revision": "2229b9f13bfe8aa444cd",
+    "url": "/homepage/static/js/2.16930bea.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/homepage/static/js/2.16930bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adbb3824a4d709caf00e",
+    "url": "/homepage/static/js/main.132c9a33.chunk.js"
+  },
+  {
+    "revision": "06b6cab4d1941f511a77",
+    "url": "/homepage/static/js/runtime-main.1bda1dc6.js"
+  }
+]);
