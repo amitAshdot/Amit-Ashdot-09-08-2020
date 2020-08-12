ee0af04fbe47a3751bccde8ba17d8799078bb723
Amit Ashdot <amitashdot@gmail.com>
2020-08-12 16:12:02 +0300

    finishing 4

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -2,13 +2,16 @@ import React from 'react'
 import HeaderNav from './HeaderNav';
 import HeaderSettings from './HeaderSettings';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom'
 
 const Header = () => {
+    let history = useHistory()
+
     const settings = useSelector(state => state.settingsReducer);
     const modeClass = settings.darkMode ? "mode--dark--header" : ""
     return (
         <div className={`header ${modeClass}`}>
-            <p className="header__logo">Weather-app</p>
+            <p className="header__logo" onClick={() => history.push('/')}>Weather-app</p>
             <HeaderSettings />
             <HeaderNav />
         </div>
